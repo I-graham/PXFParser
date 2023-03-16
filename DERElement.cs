89f2c7306aa1db6e02b1161f2d0e6fcda133a113
Ian Graham Martinez <iangraham28@gmail.com>
2023-03-16 16:28:19 +0000

    ASN.1 Parsing completed

--- DERElement.cs
+++ DERElement.cs
@@ -1,28 +1,12 @@
 ﻿namespace PFXParser
 {
-	public enum Tag : byte
-	{
-		EOC                 = 0x00,
-		BOOLEAN             = 0x01,
-		INTEGER             = 0x02,
-		BIT_STRING          = 0x03,
-		OCTET_STRING        = 0x04,
-		NULL                = 0x05,
-		OBJECT_IDENTIFIER   = 0x06,
-		UTF8_STRING         = 0x0C,
-		SEQUENCE            = 0x30,
-		SET                 = 0x31,
-        
-
-	}
-
 	public class DERElement
 	{
 		private Tag tag;
 		private List<DERElement> children = new List<DERElement>();
 		private ArraySegment<byte> data;
 
-		public static DERElement parse(ArraySegment<byte> raw)
+		public static (int, DERElement) parse(ArraySegment<byte> raw)
 		{
 			int offset = 0;
 
@@ -32,10 +16,10 @@
 			byte len_byte = raw[offset++];
 			int length = 0;
 
-			if ((len_byte & 0xA0) == 0) length = (byte)len_byte;
+			if ((len_byte & 0x80) == 0) length = (byte)len_byte;
 			else
 			{
-				byte len_size = (byte)(len_byte & ~0xA0);
+				byte len_size = (byte)(len_byte & ~0x80);
 				if (len_size > 4)
 				{
 					//Panic
@@ -46,46 +30,60 @@
 					for (uint i = 0; i < len_size; i++)
 					{
 						length <<= 8;
-						length = (byte)raw[offset++];
+						length += raw[offset++];
 					}
 				}
 			}
 
 			int start = offset;
 			int end = start + length;
+			result.data = raw.Slice(start, length);
 
-			switch (result.tag)
+			if (result.tag == Tag.SET
+				|| result.tag == Tag.SEQUENCE
+				|| ((0x20) & (int)(result.tag)) != 0)
 			{
-				case Tag.SET:
-				case Tag.SEQUENCE:
-					{
-						while (offset < end)
-						{
-							DERElement child = parse(raw.Slice(offset, end));
-							offset += child.data.Count;
-							result.children.Add(child);
-						}
-
-					}
-					break;
-
-				default:
-
-					break;
+				while (offset < end)
+				{
+					var (new_end, child) = parse(raw.Slice(offset, end-offset));
+					offset += new_end;
+					result.children.Add(child);
+				}
 			}
+				
 
-			result.data = raw.Slice(start, end);
-			return result;
+			return (end, result);
 
 		}
 
         public override string ToString() {
 
-            string output = this.tag.ToString() + ": ";
+            string output = this.tag.ToString() + ": " + this.data;
 
-            foreach (var child in children) output += "\n " + child.ToString();
+			switch(this.tag) {
+				default:
+					foreach (var child in children) output += "\n " + child.ToString();
+					break;
+			}
 
             return output;
         }
+	}
+
+	public enum Tag : byte
+	{
+		EOC                 = 0x00,
+		BOOLEAN             = 0x01,
+		INTEGER             = 0x02,
+		BIT_STRING          = 0x03,
+		OCTET_STRING        = 0x04,
+		NULL                = 0x05,
+		OBJECT_IDENTIFIER   = 0x06,
+		UTF8_STRING         = 0x0C,
+		PRINTABLE_STRING	= 0x13,
+		IA5_STRING			= 0x16,
+		BMP_STRING			= 0x1E,
+		SEQUENCE            = 0x30,
+		SET                 = 0x31,
 	}
 }
